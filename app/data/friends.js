0e75b09c5b1c4a073abcf254f6e6d99ebfd31fe7
Jake Krayger <jkrayg@protonmail.com>
2020-05-05 21:41:09 -0400

    navigation and json file working

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+    {
+        name:"Plankton",
+        photo:"https://vignette.wikia.nocookie.net/spongefan/images/7/78/Sheldon_J._Plankton.png/revision/latest/scale-to-width-down/220?cb=20190821163513",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+]
+
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,14 @@
 var friendsData = require("../data/friends");
 
 
-app.get("/api/survey", function(req, res) {
-  res.json(friendsData);
-});
+module.exports = function (app) {
 
-// API POST Requests
-// Below code handles when a user submits a form and thus submits data to the server.
-// In each of the below cases, when a user submits form data (a JSON object)
-// ...the JSON is pushed to the appropriate JavaScript array
-// (ex. User fills out a reservation request... this data is then sent to the server...
-// Then the server saves the data to the tableData array)
-// ---------------------------------------------------------------------------
+  app.get("/api/survey", function (req, res) {
+    res.json(friendsData);
+  });
 
-//app.post("/api/survey", function(req, res) {
-  // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-  // It will do this by sending out the value "true" have a table
-  // req.body is available since we're using the body parsing middleware
-  //friendsData.push(req.body);
-  //res.json(true);
-//});
+  app.post("/api/survey", function (req, res) {
+    friendsData.push(req.body);
+    res.json(true);
+  });
+}
